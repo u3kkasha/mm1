3f774291d5c6e08b7f74c0d8276b0d5bb6c475d8
abuAbdirRahman <abuAbdirRahman@f86a8ba0-6c4d-45a7-88c0-707f301703b1>
2021-11-02 03:41:29 +0000

    Server now uses an optional<Job>

--- reusable-components/Server.cc
+++ reusable-components/Server.cc
@@ -1,76 +1,64 @@
 #ifndef SERVER_H
-
 #define SERVER_H
 
 #include "Job.cc"
-
 #include "Log.hpp"
-
 #include "Timing_unit.cc"
+#include <optional>
 
-class Server {
+class Server final
+{
+private:
+  bool current_status; // true for busy false for idle
+  Log history;
+  std::optional<Job> job_being_served_currently;
 
 public:
-  Job &get_job_being_served_currently() { return job_being_served_currently; }
-
-  void set_job_being_served_currently(Job new_job) {
+  auto extract_serviced_job() 
+  { 
+    return std::move(job_being_served_curently).value(); 
+  }
+  
+  void assign_job(Job new_job) 
+  {
     job_being_served_currently = new_job;
   }
 
   bool is_busy()
-
   {
-
     bool v = (current_status) ? true : false;
-
     return v;
   }
 
   bool is_idle()
-
   {
-
     bool v = (current_status) ? false : true;
-
     return v;
   }
 
-  Log get_log() { return history; }
+  Log get_log() 
+  { 
+      return history;
+  }
 
   void update_history_log()
-
   {
-
     auto current_time = Singleton::Timing_unit::get_current_time();
-
     history.add_datum(std::make_pair(current_time, current_status));
   }
 
   void activate()
-
   {
-
     current_status = true;
-
     update_history_log();
   }
 
   void deactivate()
-
   {
-
     current_status = false;
-
     update_history_log();
   }
 
-protected:
-private:
-  bool current_status; // true for busy // false for idle
-
-  Log history;
-
-  Job job_being_served_currently;
 };
 
 #endif // SERVER_H
